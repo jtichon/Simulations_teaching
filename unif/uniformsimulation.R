48c2e75fcbe21eefd9b7e058815d36ab0d3215ae
jtichon <jenna.tichon@umanitoba.ca>
2019-06-06 13:03:38 -0500

    Add line to plot

--- unif/uniformsimulation.R
+++ unif/uniformsimulation.R
@@ -6,20 +6,22 @@ simulation<-function(n, print)
 {
   #Generate n random uniform observations on (7,13)
   obs<-runif(n, 7, 13)
-  
+
   #If print=T, print vector of observations and vector of T/F values
   if(print==T){
     print(obs)
     print((obs>=9)==TRUE)
   }
-  
+
   #Count proportion where >=9 is true out of n observations
   sum((obs>=9&obs<=13)==TRUE)/n
-  
+
 }
 
-#Test for larger sample sizes
+#Test for sample of size 10 with output printed.
 simulation(10,T)
+
+#Test for larger sample sizes
 simulation(100,F)
 simulation(10000,F)
 simulation(100000,F)
@@ -30,15 +32,15 @@ prop<-vector('numeric')
 
 #Create a vector with 1000 instances of function with sample size increasing by 100
 for(k in 1:1000){
-  
+
   prop<-c(prop,simulation(100*k,F))
-  
+
 }
- 
+
 #Create a vector of possible sample sizes
 num<-c(1:1000)
 num<-num*100
 
 #Plot sample size vs proportion, "l" option says to plot using a line
-plot(num, prop, type="l")
-
+plot(num, prop, type="l", lheight=1.5)
+abline(h=(2/3), col="red")
